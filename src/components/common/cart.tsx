db071ae6e35cad55554dbb567cf9202c3d5c6638
AbimaelSantos8 <abimael.150@hotmail.com>
2025-08-14 23:11:27 -0300

    feat: ensure shipping addres can be created

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -78,7 +78,9 @@ export const Cart = () => {
               </div>
 
               <Button className="mt-5 rounded-full" asChild>
-                <Link href="/cart/identification">Finalizar compra</Link>
+                <Link href="/cart/identification">
+                  <Link href="/cart/identification">Finalizar compra</Link>
+                </Link>
               </Button>
             </div>
           )}
